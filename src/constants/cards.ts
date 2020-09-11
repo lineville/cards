9c9f2b6be477986a0136a2d32cfa7965d89fa905
liamneville <lneville@quantumscape.com>
2020-09-11 00:40:09 -0700

    added cards

---
+++ src/constants/cards.ts
@@ -0,0 +1,71 @@
+import { Card } from '../Card'
+export const cards: Card[] = [
+  {
+    header: 'Javascript',
+    subheader: 'The language of the web',
+    avatar: 'javascript-logo.png',
+    title: 'Developer',
+    name: 'Liam Neville',
+    tags: ['HTML', 'CSS', 'JavaScript', 'React'],
+  },
+  {
+    header: 'Rust',
+    subheader: 'Safe, fast and friendly systems language',
+    avatar: 'rust-icon.svg',
+    title: 'Developer',
+    name: 'Liam Neville',
+    tags: ['Systems', 'Mozilla', 'Rust'],
+  },
+  {
+    header: 'Java',
+    subheader:
+      'The popular, verbose and modular backend language of the industry',
+    avatar: 'avatar.png',
+    title: 'Developer',
+    name: 'Liam Neville',
+    tags: ['Spring', 'Maven', 'TomCat'],
+  },
+  {
+    header: 'Scheme',
+    subheader: 'Functional, easy-to-learn academic language',
+    avatar: 'avatar.png',
+    title: 'Developer',
+    name: 'Liam Neville',
+    tags: ['Racket', 'Scheme', 'Functional'],
+  },
+  {
+    header: 'Python',
+    subheader:
+      'The simple scripting language that dominates ML, AI and data science',
+    avatar: 'avatar.png',
+    title: 'Developer',
+    name: 'Liam Neville',
+    tags: ['Python', 'ML', 'AI'],
+  },
+  {
+    header: 'TypeScript',
+    subheader:
+      "JavaScript with Types and much more. Don't do web-dev on hard-mode.",
+    avatar: 'avatar.png',
+    title: 'Developer',
+    name: 'Liam Neville',
+    tags: ['TypeScript', 'JavaScript'],
+  },
+  {
+    header: 'Ruby',
+    subheader:
+      'Object orientation to the extreme! Super high level and easy to read',
+    avatar: 'avatar.png',
+    title: 'Developer',
+    name: 'Liam Neville',
+    tags: ['Ruby', 'Rails'],
+  },
+  {
+    header: 'C#',
+    subheader: "Build production apps fast with Microsoft's great tooling",
+    avatar: 'avatar.png',
+    title: 'Developer',
+    name: 'Liam Neville',
+    tags: ['.NET Core', 'WinForms', 'C#'],
+  },
+]
